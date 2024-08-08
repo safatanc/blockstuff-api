4843fd1be5f545599d704c2164747421b13da07e
agilistikmal <agilistikmal3@gmail.com>
2024-08-08 12:55:50 +0700

    SendVerifyCode

--- internal/domain/auth/routes.go
+++ internal/domain/auth/routes.go
@@ -18,7 +18,7 @@ func (r *Routes) Init() {
 	r.Mux.HandleFunc("POST /auth/verify", r.Controller.Verify)
 	r.Mux.HandleFunc("POST /auth/login", r.Controller.Login)
 	r.Mux.HandleFunc("POST /auth/register", r.Controller.Register)
-	r.Mux.HandleFunc("POST /auth/verify/email/{email}", r.Controller.VerifyEmail)
+	r.Mux.HandleFunc("POST /auth/verify/email/{email}", r.Controller.SendVerifyCode)
 	r.Mux.HandleFunc("PUT /auth/verify/email/{email}/{code}", r.Controller.VerifyEmail)
 	r.Mux.HandleFunc("PUT /auth/reset/password/verify/{email}/{code}/{new_password}", r.Controller.ResetPasswordVerify)
 }

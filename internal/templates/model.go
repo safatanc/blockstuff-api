b597b96fc5f35c85c88d05bfbdbca468cb014c4f
agilistikmal <agilistikmal3@gmail.com>
2024-08-17 19:11:40 +0700

    Add: Transaction Email Template

---
+++ internal/templates/model.go
@@ -0,0 +1,10 @@
+package templates
+
+type EmailTransaction struct {
+	OrderID        string
+	Status         string
+	Subtotal       string
+	TransactionURL string
+	Date           string
+	Year           string
+}
